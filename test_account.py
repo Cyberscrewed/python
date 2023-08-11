381d0ca7b215950a5801779d7ad315343217194a
Cyberscrewed <142054999+Cyberscrewed@users.noreply.github.com>
2023-08-11 18:22:52 -0500

    Create test_account.py

---
+++ test_account.py
@@ -0,0 +1,45 @@
+import pytest
+from account import *
+
+class Test:
+    def setup_method(self):
+        self.a1 = Account('Jane')
+
+    def teardown_method(self):
+        del self.a1
+
+    def test_init(self):
+        assert self.a1.get_name() == 'Jane'
+        assert self.a1.get_balance() == pytest.approx(0)
+    
+    def test_deposit(self):
+        assert self.a1.deposit(-4) is False
+        assert self.a1.get_balance == pytest.approx(0)
+
+        assert self.a1.deposit(0) is False
+        assert self.a1.get_balance == pytest.approx(0)
+
+        assert self.a1.deposit(1.0) is True
+        assert self.a1.get_balance == pytest.approx(1.0)
+
+    
+    def test_withdraw(self):
+        assert self.a1.withdraw(-5) is False
+        assert self.a1.get_balance == pytest.approx(0)
+
+        assert self.a1.withdraw(0) is False
+        assert self.a1.get_balance == pytest.approx(0)
+
+        assert self.a1.withdraw(150) is False
+        assert self.a1.get_balance == pytest.approx(0)
+        self.a1.deposit(10)
+        
+        assert self.a1.withdraw(5.5) is True
+        assert self.a1.get_balance == pytest.approx(4.5)
+
+
+    
+
+
+    
+
