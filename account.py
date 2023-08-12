0d8be9250e6a2496558da1d415e16a6d882d2481
Cyberscrewed <142054999+Cyberscrewed@users.noreply.github.com>
2023-08-11 20:54:07 -0500

    Update account.py

--- account.py
+++ account.py
@@ -1,10 +1,22 @@
 class Account:
-    def __init__(self, name):
+    '''
+    Class for creating an account holder.
+    '''
+    def __init__(self, name: str) -> None:
+        '''
+        Method to initialize account holder.
+        name: name of account holder.
+        '''
         self.__account_name = name
         self.__account_balance = 0
     
 
-    def deposit(self, amount: float):
+    def deposit(self, amount: float) -> bool:
+        '''
+        Method to deposit funds into account.
+        amount: amount deposited.
+        return: value validation checker.
+        '''
         if amount > 0:
             self.__account_balance += amount
             return True
@@ -12,17 +24,30 @@ class Account:
             return False
     
 
-    def withdraw(self, amount: float):
+    def withdraw(self, amount: float) -> bool:
+        '''
+        Method to withdraw funds from account.
+        amount: amount withdrawn.
+        return: value validation checker.
+        '''
         if amount > 0 and amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         else:
             return False
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        '''
+        Method to retrieve current account balance.
+        return: account balance.
+        '''
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+        '''
+        Method to retrieve account holder name.
+        return: account holder name.
+        '''
         return self.__account_name
     
 
